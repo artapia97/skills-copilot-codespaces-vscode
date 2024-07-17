7ed508e769dc1f14123689d07a13355635281e28
Anibal Tapia <34882083+artapia97@users.noreply.github.com>
2024-07-17 02:28:19 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'MongoDB', 'Express'];
+  var members = ['John', 'Jane', 'Jim', 'Jill', 'Jack', 'Jerry'];
+  return { skills, members };
+}
